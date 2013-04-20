90716f2a05a4382eddefee77de0db8b2d8a3c3d9
Thierry <devbieres@lafamillebn.net>
2013-04-20 22:39:18 +0200

    Changement vers la bonne adresse mail.

--- Command/BaseCommand.php
+++ Command/BaseCommand.php
@@ -3,14 +3,14 @@ namespace DevBieres\Common\BaseBundle\Command;
 /*
  * ----------------------------------------------------------------------------
  * « LICENCE BEERWARE » (Révision 42):
- * <nantesparcours@lafamillebn.net> a créé ce fichier. Tant que vous conservez cet avertissement,
+ * <devbieres@lafamillebn.net> a créé ce fichier. Tant que vous conservez cet avertissement,
  * vous pouvez faire ce que vous voulez de ce truc. Si on se rencontre un jour et
  * que vous pensez que ce truc vaut le coup, vous pouvez me payer une bière en
  * retour. 
  * ----------------------------------------------------------------------------
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * <nantesparcours@lafamillebn.net> wrote this file. As long as you retain this notice you
+ * <devbieres@lafamillebn.net> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return. 
  * ----------------------------------------------------------------------------
--- Entity/GPSBase.php
+++ Entity/GPSBase.php
@@ -3,14 +3,14 @@ namespace DevBieres\Common\BaseBundle\Entity;
 /*
  * ----------------------------------------------------------------------------
  * « LICENCE BEERWARE » (Révision 42):
- * <thierry<at>lafamillebn<point>net> a créé ce fichier. Tant que vous conservez cet avertissement,
+ * <devbieres@lafamillebn.net> a créé ce fichier. Tant que vous conservez cet avertissement,
  * vous pouvez faire ce que vous voulez de ce truc. Si on se rencontre un jour et
  * que vous pensez que ce truc vaut le coup, vous pouvez me payer une bière en
  * retour. 
  * ----------------------------------------------------------------------------
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * <thierry<at>lafamillebn<point>net> wrote this file. As long as you retain this notice you
+ * <devbieres@lafamillebn.net> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return. 
  * ----------------------------------------------------------------------------
